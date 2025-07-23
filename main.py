6e16625f140714cbdd7cce68071bd457c87c1b2f
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-07-23 12:26:15 +0300

    Add missing newlines and ensure proper structure in main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ from game_project import GameProject
 
 def main():
     member1 = TeamMember("Jakes", "Designer")
+    member2 = TeamMember("Kim" , "Developer")
+
+    design_tool1 = GameDesigner("Figma")
+    design_tool2 = GameDesigner("Sketch")
+
+    programming_language1 = Developer("Python")
+    programming_language1 = Developer("C#")
+
+    GameProject1 = GameProject("Bounce", {})
+
+    
