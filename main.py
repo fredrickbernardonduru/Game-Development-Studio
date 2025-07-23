d9789c5cab655f8ac942ca381e23d0424bdb99b3
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-07-23 13:27:48 +0300

    Final test

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from team_member import TeamMember
 from game_designer import GameDesigner
 from developer import Developer
 from game_project import GameProject
 
 
 def main():
-    designer = TeamMember("Jakes", "Figma")
-    developer = TeamMember("Kim" , "Python")
+    designer = GameDesigner("Jakes", "Figma")
+    developer = Developer("Kim" , "Python")
 
     project = GameProject("Bounce", designer, developer)
 
-    programming_language1 = Developer("Python")
-    programming_language2 = Developer("C#")
-
-    GameProject1 = GameProject("Bounce", {})
-
-
-    print(member1)
-    print(member2)
-    print(design_tool1)
-    print(design_tool2)
-    print(programming_language1)
-    print(programming_language2)
-    print(GameProject1)
-
+    print(designer.introduce())
+    print(designer.create_mockup())
+    print(developer.introduce())
+    print(developer.write_code())
+    print(project.start_project())
+    print(project.launch())
+    
 if __name__ == "__main__":
         main()
 
